d3a3a38693da80ede4a8ca4a8d297b082d42b139
Jack Brashier <jmb280676@email.vccs.edu>
2017-09-03 18:44:44 -0400

    Update AssignmentTwo.java

--- AssignmentTwo.java
+++ AssignmentTwo.java
@@ -3,15 +3,39 @@ import java.util.Scanner;
  * Name: Jack Brashier
  * Date: 08/27/2017
  */
-public class AssignmentTwo {
-    public static void main(String [] args) {
+public class AssigTwo {
+    public static void main(String[] args) {
         System.out.println("Please input a number between 0 and 1000:");
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        int hundreds = input / 100;
-        int tens = (input % 100) / 10;
-        int ones = input % 10;
-        int total = hundreds + tens + ones;
-        System.out.println("The total of all components of " + input + " is " + total + ".");
+        String inputString;
+        boolean failed = false;
+        String endProg = "no";
+        while (endProg == "no") {
+            inputString = scanner.next();
+            if (inputString.length() >= 4) {
+                System.out.println("Please input a number between 0 and 1000:");
+            }
+            if (inputString.length() <= 3) {
+                for (int i = 0; i < 3; i++) {
+                    if (Character.isDigit(inputString.charAt(i)) == false) {
+                        failed = true;
+                    } else if (Character.isDigit(inputString.charAt(0)) == true && Character.isDigit(inputString.charAt(1)) == true && Character.isDigit(inputString.charAt(2)) == true) {
+                        failed = false;
+                    }
+                }
+                for (int x = 0; x < inputString.length(); x++) {
+                    if (failed == true && x == 2) {
+                        System.out.println("Please input a number between 0 and 1000:");
+                    } else if (Character.isDigit(inputString.charAt(x)) == true && x == 2) {
+                        int hundreds = Integer.parseInt(inputString) / 100;
+                        int tens = (Integer.parseInt(inputString) % 100) / 10;
+                        int ones = Integer.parseInt(inputString) % 10;
+                        int total = hundreds + tens + ones;
+                        System.out.println("The sum of all the digits in " + inputString + " is " + total + ".");
+                        endProg = "yes";
+                    }
+                }
+            }
+        }
     }
 }
